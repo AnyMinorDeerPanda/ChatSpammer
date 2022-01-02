9f95e03bf95ed42f3d0ccb146055ce333fdd76e1
Xtract <xtractgamer@gmail.com>
2022-01-02 15:38:22 -0700

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,4 +1,12 @@
 main = function(window)
+  script.Parent.MouseButton1Click:Connect(function()
+    if (window.Contents.Settings.StartButton.Text == "Start") then
+      window.Contents.Settings.StartButton.Text = "Stop"
+    else
+      window.Contents.Settings.StartButton.Text = "Start"
+    end
+  end)
+  
   while (true) do
     if (window.Contents.Settings.StartButton.Text == "Stop") then
       game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(window.Contents.Text.TextBox.Text,"LocalPlayer")
