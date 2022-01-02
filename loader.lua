532ee16d25add1bf2e6428aae6e634fa8b5a4e22
Xtract <xtractgamer@gmail.com>
2022-01-02 16:09:38 -0700

    Update and rename loader.lua to loader

--- loader.lua
+++
@@ -1,8 +0,0 @@
-return (function()
-  
-  -- Load Window Renderer
-  local window = loadstring(game:HttpGet('https://raw.githubusercontent.com/AnyMinorDeerPanda/ChatSpammer/main/WindowRenderer.lua'))();
-
-  -- Load Main Script
-  loadstring(game:HttpGet('https://raw.githubusercontent.com/AnyMinorDeerPanda/ChatSpammer/main/MainScript.lua'))(window);
-end);
