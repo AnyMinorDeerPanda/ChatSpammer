b56b85ac394b2f3e32a18e7541a9f4a248997e70
Xtract <xtractgamer@gmail.com>
2022-01-02 15:34:56 -0700

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,6 +1,9 @@
 main = function(window)
   while (true) do
-    
+    if (window.Contents.Settings.StartButton.Text == "Stop") then
+      game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(window.Contents.Text.TextBox.Text,"LocalPlayer")
+      wait(window.Contents.Settings.TextBox.Text)
+    end
   end
 end
 
